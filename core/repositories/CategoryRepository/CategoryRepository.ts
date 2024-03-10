3344b7a1ca83786cb9623137d8e516fa8f734bc7
hhuziuk <huziukwork@gmail.com>
2024-03-10 12:57:48 +0100

    added interface for CategoryRepository

---
+++ core/repositories/CategoryRepository/CategoryRepository.ts
@@ -0,0 +1,8 @@
+import {Category} from "../../domain/Category";
+
+export interface CategoryRepository {
+    addCategory(name: string): Promise<Category>;
+    getAllCategories(): Promise<Category>;
+    getCategory(id: string): Promise<Category | null>;
+    deleteCategory(id: string): Promise<void>;
+}
