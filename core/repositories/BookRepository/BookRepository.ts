f95c37d77d284810465edc7d3e0b35a6301fcf9f
hhuziuk <huziukwork@gmail.com>
2024-03-10 10:20:59 +0100

    added interface for BookRepository

---
+++ core/repositories/BookRepository/BookRepository.ts
@@ -0,0 +1,20 @@
+import {Book} from "../../domain/Book";
+
+export interface BookRepository {
+    addBook(
+        bookName: string,
+        author: string,
+        categoryId: string,
+        publisherId: string,
+        ratingId: string,
+        description: string,
+        ISBN: string,
+        language: string,
+        size: string,
+        price: number,
+    ) : Promise<Book>,
+    getBook(id: string) : Promise<Book | null>,
+    getAllBooks() : Promise<Book | null>,
+    updateBook(id: string) : Promise<Book | null>,
+    deleteBook(id: string) : Promise<void>
+}
