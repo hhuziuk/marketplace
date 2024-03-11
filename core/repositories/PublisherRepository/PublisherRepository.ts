93403782919f57fbc903e00dd618d0a44603b86d
hhuziuk <huziukwork@gmail.com>
2024-03-11 13:07:48 +0100

    created PublisherDomainService in ./core/services

--- core/repositories/PublisherRepository/PublisherRepository.ts
+++ core/repositories/PublisherRepository/PublisherRepository.ts
@@ -1,12 +1,9 @@
 import {Publisher} from "../../domain/Publisher";
 
 export interface PublisherRepostiory {
-    addPublisher(
-        category: string,
-        bookId: string,
-        categoryId: string,
-    ): Promise<Publisher>;
+    addPublisher(publisherName: string,): Promise<Publisher>;
     getAll(): Promise<Publisher[]>;
-    getById(id: string): Promise<Publisher | null>;
-    delete(id: string): Promise<void>;
+    getById(publisherId: string): Promise<Publisher | null>;
+    getByName(publisherName: string): Promise<Publisher | null>;
+    delete(publisherId: string): Promise<void>;
 }
---
+++ core/services/PublisherDomainService.ts
@@ -0,0 +1,21 @@
+import {PublisherRepostiory} from "../repositories/PublisherRepository/PublisherRepository";
+import {Publisher} from "../domain/Publisher";
+
+export class PublisherDomainService implements PublisherRepostiory {
+    constructor(private publisherDomainRepository : PublisherRepostiory) {}
+    async addPublisher(publisherName: string,): Promise<Publisher> {
+        return await this.publisherDomainRepository.addPublisher(publisherName);
+    }
+    async getAll(): Promise<Publisher[]> {
+        return await this.publisherDomainRepository.getAll();
+    }
+    async getById(publisherId: string): Promise<Publisher | null>{
+        return await this.publisherDomainRepository.getById(publisherId);
+    }
+    async getByName(publisherName: string): Promise<Publisher | null>{
+        return await this.publisherDomainRepository.getByName(publisherName);
+    }
+    async delete(publisherId: string): Promise<void> {
+        return await this.publisherDomainRepository.delete(publisherId);
+    }
+}
