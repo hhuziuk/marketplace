757357d5c83308d39ba9bfad834d18fda3e96b92
hhuziuk <huziukwork@gmail.com>
2024-03-13 14:32:39 +0100

    created routers in ./infrastructure/routers

--- core/services/WishlistDomainService.ts
+++
@@ -1,8 +0,0 @@
-import {WishListRepository} from "../repositories/WishlistRepository/WishlistRepository";
-
-export class WishlistDomainService implements WishListRepository {
-    constructor(private wishlistDomainRepository : WishListRepository){}
-    async cleanWishList() : Promise<void> {
-        return await this.wishlistDomainRepository.cleanWishList();
-    }
-}
