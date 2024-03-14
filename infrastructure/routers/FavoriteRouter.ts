20d1ce1bc8684b38878bd295046aa717d21745df
hhuziuk <huziukwork@gmail.com>
2024-03-14 13:35:18 +0100

    updated FavoriteRouter in ./infrastructure/routers

--- infrastructure/routers/FavoriteRouter.ts
+++ infrastructure/routers/FavoriteRouter.ts
@@ -1,4 +1,8 @@
 import express from "express"
 const router = express.Router();
 
+router.post('/add', ) // ---> addToFavorite()
+router.get('/favorites', ) // ---> getAll() for implementation!
+router.delete('/delete', ) // ---> deleteFromFavorite()
+
 export default router
