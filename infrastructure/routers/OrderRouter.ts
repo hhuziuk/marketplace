90dc9f5c0310e73ae0f41a475ceacb72f6b9d537
hhuziuk <huziukwork@gmail.com>
2024-03-14 13:42:52 +0100

    updated OrderRouter in ./infrastructure/routers

--- infrastructure/routers/OrderRouter.ts
+++ infrastructure/routers/OrderRouter.ts
@@ -1,4 +1,12 @@
 import express from "express"
 const router = express.Router();
 
+router.post('/add', ) // ---> createOrder()
+router.post('/confirm', ) // ---> confirmOrder()
+router.get('/orders', ) // ---> getAll() for implementation!
+router.get('/:orderId', ) // ---> getById() for implementation!
+router.get('/:bookAuthor', ) // ---> getByAuthor() for implementation!
+router.update('/setStatus', ) // ---> setStatus()
+router.delete('/cancelOrder', ) // ---> cancelOrder()
+
 export default router
