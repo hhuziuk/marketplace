4b9e9b9677d5723353b40611a8461c21b9cace8f
hhuziuk <huziukwork@gmail.com>
2024-03-16 13:13:27 +0100

    updated CategoryRouter.ts

--- infrastructure/routers/CategoryRouter.ts
+++ infrastructure/routers/CategoryRouter.ts
@@ -3,10 +3,10 @@ const router = express.Router();
 
 router.post('/add', ) // ---> addPublisher()
 router.get('/:publisherId', ) // ---> getById()
-router.get('/:publisherName', ) // ---> getByName()
+router.get('/name/:publisherName', ) // ---> getByName()
 router.get('/', ) // ---> getAll()
-router.put('/', ) // ---> ???? update() for implementation
-router.delete('/:id', ) // ---> delete()
+router.put('/:publisherId', ) // ---> ???? update() for implementation
+router.delete('/:publisherId', ) // ---> delete()
 
 
 export default router
