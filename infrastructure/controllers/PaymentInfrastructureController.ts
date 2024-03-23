ae5ddbaca45c8dbe9da3824e615963392d6bd40d
hhuziuk <huziukwork@gmail.com>
2024-03-23 21:16:17 +0100

    added basis for functions in PublisherInfrastructureController, PaymentInfrastructureController

--- infrastructure/controllers/PaymentInfrastructureController.ts
+++ infrastructure/controllers/PaymentInfrastructureController.ts
@@ -1,9 +1,8 @@
 import logger from "../../tools/logger";
 import {NextFunction} from "express";
-
 class PaymentInfrastructureController {
     constructor(readonly paymentService: any = PaymentInfrastructureService) {}
-    async createOrder(req: Request, res: Response, next: NextFunction){
+    async setPaymentMethod(req: Request, res: Response, next: NextFunction){
         try{
             return res.json()
         } catch(e){
@@ -11,7 +10,21 @@ class PaymentInfrastructureController {
             logger.error(e)
         }
     }
-
+    async getAll(req: Request, res: Response, next: NextFunction){
+        try{
+            return res.json()
+        } catch(e){
+            next(e);
+            logger.error(e)
+        }
+    }
+    async updatePaymentMethod(req: Request, res: Response, next: NextFunction){
+        try{
+            return res.json()
+        } catch(e){
+            next(e);
+            logger.error(e)
+        }
+    }
 }
-
 export default new PaymentInfrastructureController();
---
+++ infrastructure/controllers/PublisherInfrastructureController.ts
@@ -0,0 +1,63 @@
+import logger from "../../tools/logger";
+import {NextFunction} from "express";
+
+class PublisherInfrastructureController {
+    constructor(readonly publisherService: any = PublisherInfrastructureService) {}
+    async addPublisher(req: Request, res: Response, next: NextFunction){
+        try{
+            return res.json()
+        } catch(e){
+            next(e);
+            logger.error(e)
+        }
+    }
+    async getAll(req: Request, res: Response, next: NextFunction){
+        try{
+            return res.json()
+        } catch(e){
+            next(e);
+            logger.error(e)
+        }
+    }
+    async getById(req: Request, res: Response, next: NextFunction){
+        try{
+            return res.json()
+        } catch(e){
+            next(e);
+            logger.error(e)
+        }
+    }
+    async getByName(req: Request, res: Response, next: NextFunction){
+        try{
+            return res.json()
+        } catch(e){
+            next(e);
+            logger.error(e)
+        }
+    }
+    async save(req: Request, res: Response, next: NextFunction){
+        try{
+            return res.json()
+        } catch(e){
+            next(e);
+            logger.error(e)
+        }
+    }
+    async update(req: Request, res: Response, next: NextFunction){
+        try{
+            return res.json()
+        } catch(e){
+            next(e);
+            logger.error(e)
+        }
+    }
+    async delete(req: Request, res: Response, next: NextFunction){
+        try{
+            return res.json()
+        } catch(e){
+            next(e);
+            logger.error(e)
+        }
+    }
+}
+export default new PublisherInfrastructureController();
