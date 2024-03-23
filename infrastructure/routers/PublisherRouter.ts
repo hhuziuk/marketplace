024e7c88e1704e7a4f32e1877e180e3aee99a0f9
hhuziuk <huziukwork@gmail.com>
2024-03-23 22:04:21 +0100

    updated PublisherRouter.ts

--- infrastructure/routers/PublisherRouter.ts
+++ infrastructure/routers/PublisherRouter.ts
@@ -1,13 +1,14 @@
 import express from "express"
 import {checkRole} from "../middleware/RoleMiddleware";
 import {Role} from "../../core/domain/enums/Role";
+import PublisherInfrastructureController from "../controllers/PublisherInfrastructureController";
 const router = express.Router();
 
-router.post('/add', checkRole(Role.Admin) ) // ---> addPublisher()
-router.get('/publisher/:publisherId', ) // ---> getById()
-router.get('/publisher/:publisherName', ) // ---> getByName()
-router.get('/', ) // ---> getAll()
-router.put('/publisher/:publisherId', checkRole(Role.Admin) ) // ---> ???? update() for implementation
-router.delete('/publisher/:publisherId', checkRole(Role.Admin) ) // ---> delete()
+router.post('/add', checkRole(Role.Admin), PublisherInfrastructureController.addPublisher) // ---> addPublisher()
+router.get('/publisher/:publisherId', checkRole(Role.Admin), checkRole(Role.Seller), checkRole(Role.Customer), PublisherInfrastructureController.getById) // ---> getById()
+router.get('/publisher/:publisherName', checkRole(Role.Admin), checkRole(Role.Seller), checkRole(Role.Customer), PublisherInfrastructureController.getByName) // ---> getByName()
+router.get('/', checkRole(Role.Admin), checkRole(Role.Seller), checkRole(Role.Customer), PublisherInfrastructureController.getAll) // ---> getAll()
+router.put('/publisher/:publisherId', checkRole(Role.Admin), PublisherInfrastructureController.update) // ---> ???? update() for implementation
+router.delete('/publisher/:publisherId', checkRole(Role.Admin), PublisherInfrastructureController.delete) // ---> delete()
 
 export default router
