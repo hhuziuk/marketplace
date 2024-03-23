50fb1b047b37ae63cc73c0928b208e336fbabdf2
hhuziuk <huziukwork@gmail.com>
2024-03-23 22:02:02 +0100

    updated PaymentRouter.ts

--- infrastructure/routers/PaymentRouter.ts
+++ infrastructure/routers/PaymentRouter.ts
@@ -1,8 +1,12 @@
 import express from "express"
+import {checkRole} from "../middleware/RoleMiddleware";
+import {Role} from "../../core/domain/enums/Role";
+import PaymentInfrastructureController from "../controllers/PaymentInfrastructureController";
+
 const router = express.Router();
 
-router.post('/set', ) // ---> setPaymetMethod()
-router.get('/payments', ) // ---> getAll() for implementation!
-router.put('/update', ) // ---> setStatus()  for implementation!
+router.post('/set', checkRole(Role.Customer), PaymentInfrastructureController.setPaymentMethod) // ---> setPaymetMethod()
+router.get('/payments', checkRole(Role.Customer), PaymentInfrastructureController.getAll) // ---> getAll() for implementation!
+router.put('/update', checkRole(Role.Customer), PaymentInfrastructureController.updatePaymentMethod) // ---> setStatus()  for implementation!
 
 export default router
