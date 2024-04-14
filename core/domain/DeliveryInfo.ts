d003c63b63e2fff096ffe86953302f4d9ae9f6a4
hhuziuk <huziukwork@gmail.com>
2024-04-14 13:56:37 +0200

    added DeliveryInfoPostgresRepository.ts, ParcelPostgresRepository.ts, ShipmentPostgresRepository.ts

--- core/domain/DeliveryInfo.ts
+++
@@ -1,9 +0,0 @@
-export class DeliveryInfo {
-    constructor(
-        readonly deliveryInfoId: string,
-        readonly shipmentId: string,
-        readonly location: string,
-        readonly statusDescription: string,
-        readonly timestamp: Date,
-    ) {}
-}
