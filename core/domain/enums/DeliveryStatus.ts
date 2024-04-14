72c34ca13c7e58beb2451647e10f096b89481b23
hhuziuk <huziukwork@gmail.com>
2024-04-14 21:38:55 +0200

    wrote ShipmentInfrastructureService and ShipmentInfrastructureController

--- core/domain/enums/DeliveryStatus.ts
+++
@@ -1,8 +0,0 @@
-export enum DeliveryStatus {
-    PENDING = "Pending",
-    IN_TRANSIT = "In Transit",
-    OUT_FOR_DELIVERY = "Out for Delivery",
-    DELIVERED = "Delivered",
-    RETURNED = "Returned",
-    CANCELLED = "Cancelled",
-}
