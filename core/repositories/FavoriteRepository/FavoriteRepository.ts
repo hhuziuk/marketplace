e0275a9845e14b4d8488c09854379db95c7a59d4
hhuziuk <huziukwork@gmail.com>
2024-04-15 13:08:15 +0200

    resolved problem with password nullability in UserEntity

--- core/repositories/FavoriteRepository/FavoriteRepository.ts
+++
@@ -1,7 +0,0 @@
-import {Favorite} from "../../domain/Favorite";
-
-export interface FavoriteRepository {
-    addToFavorite(bookId: string): Promise<Favorite>,
-    getAll(): Promise<Favorite[]>;
-    deleteFromFavorite(bookId: string): Promise<void>
-}
