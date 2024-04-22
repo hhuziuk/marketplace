9454408db4f6551dbfd534573eb8136370ee9e1f
hhuziuk <huziukwork@gmail.com>
2024-04-23 00:12:13 +0200

    added swagger documentation and fixed some small mistekes like throw redundant near ApiError in controllers and services

--- infrastructure/routers/ShipmentRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import ShipmentController from '../controllers/ShipmentInfrastructureController';
-
-const router = express.Router();
-
-router.post('/shipments', ShipmentController.createShipment);
-router.get('/shipments/:trackingCode', ShipmentController.trackShipment);
-
-export default router;
